2abd7e17a4959d739c655c1a5b44d980b4eece01
fleuroostingh <94438893+fleuroostingh@users.noreply.github.com>
2021-12-14 14:39:37 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-
+ 
 /*****************/
 /* DROPDOWN MENU */
 /*****************/
@@ -6,8 +6,16 @@
 var dropdownMenu = document.querySelector ("nav");
 
 function menuOpenDicht(){
+	document.documentElement.classList.remove("loading");
 	dropdownMenu.classList.toggle("transitie_navigatie");
 }
 
 dropdownMenu.addEventListener("click", menuOpenDicht);
+
+
+window.addEventListener("scroll", function(){
+	var header = document.querySelector("header > div:first-of-type");
+	header.classList.toggle("sticky", window.scrollY > 600)
+})
+
 
